ad3a7fc6d444b396e2546f6fa18b20f3f44e7790
aman-saha <aman.saha2015@vit.ac.in>
2017-05-02 23:56:50 +0530

    sudo

--- play_sudoku/reg.js
+++ play_sudoku/reg.js
@@ -16,10 +16,123 @@ $(document).ready(function(){
 		  }
 		});
 	});
-	
+
 	$("#logout").click(function()
 	{
 	    var name = "ghost";
+	    $.ajax({
+		  type: 'POST',
+		  url: 'logout.php',
+		  data: {name:name},
+		  success: function(data){
+		  	//window.alert(data);		  	
+		  }
+		});
+	});
+	
+	$("#solution").click(function()
+	{
+	    var s11 = $("#11").val();
+	    var s12 = $("#12").val();
+	    var s13 = $("#13").val();
+	    var s14 = $("#14").val();
+	    var s15 = $("#15").val();
+	    var s16 = $("#16").val();
+	    var s17 = $("#17").val();
+	    var s18 = $("#18").val();
+	    var s19 = $("#19").val();
+
+	    var s21 = $("#21").val();
+	    var s22 = $("#22").val();
+	    var s23 = $("#23").val();
+	    var s24 = $("#24").val();
+	    var s25 = $("#25").val();
+	    var s26 = $("#26").val();
+	    var s27 = $("#27").val();
+	    var s28 = $("#28").val();
+	    var s29 = $("#29").val();
+
+	    var s31 = $("#31").val();
+	    var s32 = $("#32").val();
+	    var s33 = $("#33").val();
+	    var s34 = $("#34").val();
+	    var s35 = $("#35").val();
+	    var s36 = $("#36").val();
+	    var s37 = $("#37").val();
+	    var s38 = $("#38").val();
+	    var s39 = $("#39").val();
+
+	    var s41 = $("#41").val();
+	    var s42 = $("#42").val();
+	    var s43 = $("#43").val();
+	    var s44 = $("#44").val();
+	    var s45 = $("#45").val();
+	    var s46 = $("#46").val();
+	    var s47 = $("#47").val();
+	    var s48 = $("#48").val();
+	    var s49 = $("#49").val();
+
+	    var s51 = $("#51").val();
+	    var s52 = $("#52").val();
+	    var s53 = $("#53").val();
+	    var s54 = $("#54").val();
+	    var s55 = $("#55").val();
+	    var s56 = $("#56").val();
+	    var s57 = $("#57").val();
+	    var s58 = $("#58").val();
+	    var s59 = $("#59").val();
+
+	    var s61 = $("#61").val();
+	    var s62 = $("#62").val();
+	    var s63 = $("#63").val();
+	    var s64 = $("#64").val();
+	    var s65 = $("#65").val();
+	    var s66 = $("#66").val();
+	    var s67 = $("#67").val();
+	    var s68 = $("#68").val();
+	    var s69 = $("#69").val();
+
+	    var s71 = $("#71").val();
+	    var s72 = $("#72").val();
+	    var s73 = $("#73").val();
+	    var s74 = $("#74").val();
+	    var s75 = $("#75").val();
+	    var s76 = $("#76").val();
+	    var s77 = $("#77").val();
+	    var s78 = $("#78").val();
+	    var s79 = $("#79").val();
+
+	    var s81 = $("#81").val();
+	    var s82 = $("#82").val();
+	    var s83 = $("#83").val();
+	    var s84 = $("#84").val();
+	    var s85 = $("#85").val();
+	    var s86 = $("#86").val();
+	    var s87 = $("#87").val();
+	    var s88 = $("#88").val();
+	    var s89 = $("#89").val();
+
+	    var s91 = $("#91").val();
+	    var s92 = $("#92").val();
+	    var s93 = $("#93").val();
+	    var s94 = $("#94").val();
+	    var s95 = $("#95").val();
+	    var s96 = $("#96").val();
+	    var s97 = $("#97").val();
+	    var s98 = $("#98").val();
+	    var s99 = $("#99").val();
+
+	    var s1 = [s11,s12,s13,s14,s15,s16,s17,s18,s19];
+	    var s2 = [s21,s22,s23,s24,s25,s26,s27,s28,s29];
+	    var s3 = [s31,s32,s33,s34,s35,s36,s37,s38,s39];
+	    var s4 = [s41,s42,s43,s44,s45,s46,s47,s48,s49];
+	    var s5 = [s51,s52,s53,s54,s55,s56,s57,s58,s59];
+	    var s6 = [s61,s62,s63,s64,s65,s66,s67,s68,s69];
+	    var s7 = [s71,s72,s73,s74,s75,s76,s77,s78,s79];
+	    var s8 = [s81,s82,s83,s84,s85,s86,s87,s88,s89];
+	    var s9 = [s91,s92,s93,s94,s95,s96,s97,s98,s99];
+
+	    alert(s1);
 	    $.ajax({
 		  type: 'POST',
 		  url: 'logout.php',
--- play_sudoku/sudoku.php
+++ play_sudoku/sudoku.php
@@ -18,7 +18,9 @@
 <head>
   <meta charset="UTF-8">
   <title>Sudoku</title>  
-      <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <style type="text/css">
   header {
@@ -63,110 +65,110 @@ footer {
   <div>
   <form id="grid">
   <div id="subgrid-1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="6" disabled>
-     <input type="text" maxlength="1">
+     <input type="text" id="11" maxlength="1">
+     <input type="text" id="12" maxlength="1">
+     <input type="text" id="13" maxlength="1">
+     <input type="text" id="14" maxlength="1">
+     <input type="text" id="15" maxlength="1">
+     <input type="text" id="16" maxlength="1">
+     <input type="text" id="17" maxlength="1">
+     <input type="text" id="18" maxlength="1" value="6" disabled>
+     <input type="text" id="19" maxlength="1">
   </div>
   <div id="subgrid-2">
-     <input type="text" maxlength="1" value="1" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="3" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="7" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="5" disabled>
+     <input type="text" id="21" maxlength="1" value="1" disabled>
+     <input type="text" id="22" maxlength="1">
+     <input type="text" id="23" maxlength="1" value="3" disabled>
+     <input type="text" id="24" maxlength="1">
+     <input type="text" id="25" maxlength="1">
+     <input type="text" id="26" maxlength="1" value="7" disabled>
+     <input type="text" id="27" maxlength="1">
+     <input type="text" id="28" maxlength="1">
+     <input type="text" id="29" maxlength="1" value="5" disabled>
   </div>
   <div id="subgrid-3">
-     <input type="text" maxlength="1" value="9" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="7" disabled>
-     <input type="text" maxlength="1" value="5" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="6" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="3" disabled>
-     <input type="text" maxlength="1" value="2" disabled>
+     <input type="text" id="31" maxlength="1" value="9" disabled>
+     <input type="text" id="32" maxlength="1">
+     <input type="text" id="33" maxlength="1" value="7" disabled>
+     <input type="text" id="34" maxlength="1" value="5" disabled>
+     <input type="text" id="35" maxlength="1">
+     <input type="text" id="36" maxlength="1" value="6" disabled>
+     <input type="text" id="37" maxlength="1">
+     <input type="text" id="38" maxlength="1" value="3" disabled>
+     <input type="text" id="39" maxlength="1" value="2" disabled>
   </div>
   <div id="subgrid-4">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="8" disabled>
-     <input type="text" maxlength="1" value="3" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="2" disabled>
+     <input type="text" id="41" maxlength="1">
+     <input type="text" id="42" maxlength="1">
+     <input type="text" id="43" maxlength="1">
+     <input type="text" id="44" maxlength="1">
+     <input type="text" id="45" maxlength="1">
+     <input type="text" id="46" maxlength="1" value="8" disabled>
+     <input type="text" id="47" maxlength="1" value="3" disabled>
+     <input type="text" id="48" maxlength="1">
+     <input type="text" id="49" maxlength="1" value="2" disabled>
   </div>
   <div id="subgrid-5">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="1" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
+     <input type="text" id="51" maxlength="1">
+     <input type="text" id="52" maxlength="1">
+     <input type="text" id="53" maxlength="1">
+     <input type="text" id="54" maxlength="1">
+     <input type="text" id="55" maxlength="1" value="1" disabled>
+     <input type="text" id="56" maxlength="1">
+     <input type="text" id="57" maxlength="1">
+     <input type="text" id="58" maxlength="1">
+     <input type="text" id="59" maxlength="1">
   </div>
   <div id="subgrid-6">
-     <input type="text" maxlength="1" value="8" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="9" disabled>
-     <input type="text" maxlength="1" value="4" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
+     <input type="text" id="61" maxlength="1" value="8" disabled>
+     <input type="text" id="62" maxlength="1">
+     <input type="text" id="63" maxlength="1" value="9" disabled>
+     <input type="text" id="64" maxlength="1" value="4" disabled>
+     <input type="text" id="65" maxlength="1">
+     <input type="text" id="66" maxlength="1">
+     <input type="text" id="67" maxlength="1">
+     <input type="text" id="68" maxlength="1">
+     <input type="text" id="69" maxlength="1">
   </div>
   <div id="subgrid-7">
-     <input type="text" maxlength="1" value="4" disabled>
-     <input type="text" maxlength="1" value="8" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="6" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="1" disabled>
-     <input type="text" maxlength="1" value="2" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="5" disabled>
+     <input type="text" id="71" maxlength="1" value="4" disabled>
+     <input type="text" id="72" maxlength="1" value="8" disabled>
+     <input type="text" id="73" maxlength="1">
+     <input type="text" id="74" maxlength="1" value="6" disabled>
+     <input type="text" id="75" maxlength="1">
+     <input type="text" id="76" maxlength="1" value="1" disabled>
+     <input type="text" id="77" maxlength="1" value="2" disabled>
+     <input type="text" id="78" maxlength="1">
+     <input type="text" id="79" maxlength="1" value="5" disabled>
   </div>
   <div id="subgrid-8">
-     <input type="text" maxlength="1" value="3" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="9" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="7" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="1" disabled>
+     <input type="text" id="81" maxlength="1" value="3" disabled>
+     <input type="text" id="82" maxlength="1">
+     <input type="text" id="83" maxlength="1">
+     <input type="text" id="84" maxlength="1" value="9" disabled>
+     <input type="text" id="85" maxlength="1">
+     <input type="text" id="86" maxlength="1">
+     <input type="text" id="87" maxlength="1" value="7" disabled>
+     <input type="text" id="88" maxlength="1">
+     <input type="text" id="89" maxlength="1" value="1" disabled>
    </div>
   <div id="subgrid-9">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1" value="9" disabled>
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
-     <input type="text" maxlength="1">
+     <input type="text" id="91" maxlength="1">
+     <input type="text" id="92" maxlength="1" value="9" disabled>
+     <input type="text" id="93" maxlength="1">
+     <input type="text" id="94" maxlength="1">
+     <input type="text" id="95" maxlength="1">
+     <input type="text" id="96" maxlength="1">
+     <input type="text" id="97" maxlength="1">
+     <input type="text" id="98" maxlength="1">
+     <input type="text" id="99" maxlength="1">
    </div>
 </form>
 </div>
-  <script src="reg.js"></script>
   <footer>
-    <a class="cta" href="logout.php">Submit Solution</a>
+    <button type="button" class="cta" id ="solution">Sumbit Solution</button>
     <a class="cta" href="logout.php">Logout <?php echo "$current_username"; ?> </a>
   </footer>
+  <script src="reg.js"></script>
 </body>
 </html>
